509b57f2e83fd29fb3baa7949c9bd389bbf7f599
Daniel Chen <chendaniely@users.noreply.github.com>
2024-08-28 12:02:29 -0700

    add a type hints app example

---
+++ app/app-010-050-type_hints.py
@@ -0,0 +1,100 @@
+import pandas as pd
+from shiny import App, render, reactive, ui
+
+app_ui = ui.page_fixed(
+    ui.output_ui("table_day_filter"),
+    ui.output_ui("table_time_filter"),
+    ui.output_data_frame("render_df"),
+)
+
+def server(input, output, session):
+
+    @reactive.calc
+    def filter_idx() -> pd.Index:
+        df = df_tips()
+        idx = df.index
+
+        filters = ["filter_day", "filter_time"]
+        cols = ["day", "time"]
+
+        for fltr, col in zip(filters, cols):
+            if input[fltr]():
+                current_idx = df.loc[df[col].isin(input[fltr]())].index
+                idx = idx.intersection(current_idx)
+
+        return idx
+
+
+    # filtered dataframe from filters
+    @reactive.calc
+    def data_filtered() -> pd.DataFrame:
+        df = df_tips().loc[filter_idx()]
+        return df
+
+    # dataframe to view in app
+    @render.data_frame
+    def render_df():
+        return render.DataGrid(data_filtered())
+
+    # table day filter
+    @render.ui
+    def table_day_filter():
+        return ui.input_selectize(
+            "filter_day",
+            "table day filter:",
+            df_tips()["day"].unique().tolist(),
+            multiple=True,
+            remove_button=True,
+            options={"plugins": ["clear_button"]},
+        )
+
+    # table time filter
+    @render.ui
+    def table_time_filter():
+        return ui.input_selectize(
+            "filter_time",
+            "table time filter:",
+            df_tips()["time"].unique().tolist(),
+            multiple=True,
+            remove_button=True,
+            options={"plugins": ["clear_button"]},
+        )
+
+    # placeholder for joined dataframe
+    @reactive.calc
+    def df_tips():
+        # fmt: off
+        data = {
+            'total_bill': [
+                16.99, 10.34, 21.01, 23.68, 24.59,
+                25.29, 8.77, 26.88, 15.04, 14.78
+            ],
+            'tip': [
+                1.01, 1.66, 3.50, 3.31, 3.61,
+                4.71, 2.00, 3.12, 3.52, 3.00
+            ],
+            'sex': [
+                'Female', 'Male', 'Male', 'Male', 'Female',
+                'Male', 'Male', 'Male', 'Male', 'Female'
+            ],
+            'smoker': [
+                'No', 'No', 'No', 'No', 'Yes',
+                'No', 'No', 'Yes', 'No', 'Yes'
+            ],
+            'day': [
+                'Sun', 'Fri', 'Sun', 'Thu', 'Sun',
+                'Sun', 'Sat', 'Sat', 'Sat', 'Sat'
+            ],
+            'time': [
+                'Dinner', 'Dinner', 'Lunch', 'Dinner', 'Lunch',
+                'Dinner', 'Lunch', 'Dinner', 'Lunch', 'Dinner'
+            ],
+            'size': [2, 3, 3, 2, 4, 4, 2, 4, 2, 2]
+        }
+        # fmt: on
+
+        df = pd.DataFrame(data)
+        return df
+
+
+app = App(app_ui, server)
