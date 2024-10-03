3ffe78f8e6dbedab0f528ce017c22b8bc650c2a5
Daniel Chen <chendaniely@users.noreply.github.com>
2024-10-03 10:51:42 -0700

    incorporate feedback from blogpost, and fix apps from new package name

--- app/app-adaptive_filters/app-0010-030-overrides.py
+++ app/app-adaptive_filters/app-0010-030-overrides.py
@@ -1,23 +1,11 @@
 import pandas as pd
 from shiny import App, render, reactive, ui
 
-from pyshiny_adaptive_filter import adaptive_filter_module
-
-data = {
-    'total_bill': [16.99, 10.34, 21.01, 23.68, 24.59],
-    'tip': [1.01, 1.66, 3.50, 3.31, 3.61],
-    'sex': ['Female', 'Male', 'Male', 'Male', 'Female'],
-    'smoker': ['No', 'No', 'No', 'No', 'Yes'],
-    'day': ['Sun', 'Sun', 'Sun', 'Fri', 'Sun'],
-    'time': ['Lunch', 'Dinner', 'Dinner', 'Dinner', 'Dinner'],
-    'size': [2, 3, 3, 2, 4]
-}
-
-tips = pd.DataFrame(data)
+import shiny_adaptive_filter as af
 
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        adaptive_filter_module.filter_ui("adaptive"),
+        af.filter_ui("adaptive"),
     ),
     ui.output_data_frame("render_df"),
 )
@@ -41,11 +29,23 @@ def server(input, output, session):
     "tip": None,
     }
 
-    filter_return = adaptive_filter_module.filter_server(
+    filter_return = af.filter_server(
         "adaptive",
         df=tips_reactive,
         override=override,
     )
     filter_idx = filter_return["filter_idx"]
+
+data = {
+    'total_bill': [16.99, 10.34, 21.01, 23.68, 24.59],
+    'tip': [1.01, 1.66, 3.50, 3.31, 3.61],
+    'sex': ['Female', 'Male', 'Male', 'Male', 'Female'],
+    'smoker': ['No', 'No', 'No', 'No', 'Yes'],
+    'day': ['Sun', 'Sun', 'Sun', 'Fri', 'Sun'],
+    'time': ['Lunch', 'Dinner', 'Dinner', 'Dinner', 'Dinner'],
+    'size': [2, 3, 3, 2, 4]
+}
+
+tips = pd.DataFrame(data)
 
 app = App(app_ui, server)
